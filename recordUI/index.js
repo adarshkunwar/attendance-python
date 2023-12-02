1412dbb1ea2c015c4a133e10274b2eaa90830a33
jivan aryal <jeevanaryal7947@gmail.com>
2023-12-02 09:13:12 +0545

    added recordUI for table view

---
+++ recordUI/index.js
@@ -0,0 +1,46 @@
+import data from "../json/Attendance.json";
+
+console.log(data);
+document.addEventListener("DOMContentLoaded", function () {
+  // Sample data array, replace this with your actual data
+  const mapArrayData = [
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Admin" },
+    { id: 2, name: "Jane Doe", email: "jane@example.com", role: "User" },
+    // Add more data as needed
+  ];
+
+  const tableBody = document.getElementById("tableBody");
+  const searchInput = document.getElementById("searchInput");
+
+  // Function to render table rows based on the search input
+  function renderTable() {
+    const searchTerm = searchInput.value.toLowerCase();
+
+    // Clear existing rows
+    tableBody.innerHTML = "";
+
+    // Filter data based on the search term
+    const filteredData = mapArrayData.filter((data) =>
+      Object.values(data).some((value) =>
+        value.toString().toLowerCase().includes(searchTerm)
+      )
+    );
+
+    // Populate table rows
+    filteredData.forEach((data) => {
+      const row = document.createElement("tr");
+      Object.values(data).forEach((value) => {
+        const cell = document.createElement("td");
+        cell.textContent = value;
+        row.appendChild(cell);
+      });
+      tableBody.appendChild(row);
+    });
+  }
+
+  // Initial rendering
+  renderTable();
+
+  // Add event listener for the search input
+  searchInput.addEventListener("input", renderTable);
+});
